42cca06cf7e68c7b028ac4e301738dfe4fba20ab
Raffael Patrício de Souza <raffaelps@gmail.com>
2013-11-30 11:19:32 -0200

    Gambiarra

--- MusiKapp/MKJsonHelper.m
+++ MusiKapp/MKJsonHelper.m
@@ -11,23 +11,24 @@
 @implementation MKJsonHelper
 
 +(NSArray*)searchRepository:(NSString*)stringUrl andKey:(NSString*)key{
-    NSURL *url = [NSURL URLWithString:stringUrl];
-    // Submetendo a requisição sem o NSMutableURLRequest, assim vai com GET.
-    NSError *error;
-    NSData *resultado = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&error];
-        
-    // Verificando a ocorrência de erros HTTP.
-    if(error){
-        NSLog(@"Erro HTTP: %@", error.description);
-        return nil;
-    }
-        
-    // Acessando o elemento "data" da estrutura retornada pelo serviço.
-    NSDictionary *respostaJSON = [NSJSONSerialization JSONObjectWithData:resultado options:kNilOptions error:nil];
-    NSArray *monitoramentosJSON = [respostaJSON objectForKey:key];
+//    NSURL *url = [NSURL URLWithString:stringUrl];
+//    // Submetendo a requisição sem o NSMutableURLRequest, assim vai com GET.
+//    NSError *error;
+//    NSData *resultado = [NSData dataWithContentsOfURL:url options:NSDataReadingUncached error:&error];
+//        
+//    // Verificando a ocorrência de erros HTTP.
+//    if(error){
+//        NSLog(@"Erro HTTP: %@", error.description);
+//        return nil;
+//    }
+//        
+//    // Acessando o elemento "data" da estrutura retornada pelo serviço.
+//    NSDictionary *respostaJSON = [NSJSONSerialization JSONObjectWithData:resultado options:kNilOptions error:nil];
+//    NSMutableArray *monitoramentosJSON = [[NSMutableArray alloc] init];
     
-    return monitoramentosJSON;
-    /*NSString *codigo = @"RA705639681BR";
+    //NSArray *monitoramentosJSON = [respostaJSON objectForKey:key];
+    
+    NSString *codigo = @"RA705639681BR";
     NSString *stringUrla = [NSString stringWithFormat:@"http://services.sandbox.encomendaz.net/tracking.json?id=%@", codigo];
     NSURL *url = [NSURL URLWithString:stringUrla];
     // Submetendo a requisição sem o NSMutableURLRequest, assim vai com GET.
@@ -42,9 +43,18 @@
     
     // Acessando o elemento "data" da estrutura retornada pelo serviço.
     NSDictionary *respostaJSON = [NSJSONSerialization JSONObjectWithData:resultado options:kNilOptions error:nil];
-    NSArray *monitoramentosJSON = [respostaJSON objectForKey:@"data"];
+    NSMutableArray *monitoramentosJSON = [[NSMutableArray alloc] init];
+    //NSArray *monitoramentosJSON = [respostaJSON objectForKey:@"data"];
+    //[monitoramentosJSON addObject:[respostaJSON objectForKey:@"data"]];
     
-    return monitoramentosJSON;*/
+    id seila = [respostaJSON objectForKey:@"data"];
+    
+    if ([seila isKindOfClass:[NSArray class]])
+        monitoramentosJSON = [respostaJSON objectForKey:@"data"];
+    else
+        [monitoramentosJSON addObject:[respostaJSON objectForKey:@"data"]];
+    
+    return monitoramentosJSON;
 }
 
 @end
